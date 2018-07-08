38176c3b7126db833ec8a244a8c154df81c86566
Jen Strong <jenniferastrong@gmail.com>
2018-07-08 12:24:29 +0100

    Put posts on cards

--- README.md
+++ README.md
@@ -22,3 +22,18 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+
+
+<h1>Welcome to Postagram</h1>
+
+    <% @posts.each do |post| %>
+      <div class="card" style="width: 18rem;">
+        <div class="card-header"> **Avatar/User Here </div>
+        <img class="card-img-top" <%= image_tag(post.image, style: "width: 100%") %>
+        <div class="card-body">
+          <p class="card-text"><%= post.description %></p>
+          <div class="card-text"><%= link_to 'Edit', edit_post_path(post) %></div>
+          <div class="card-text"><%= link_to 'Delete', post, method: :delete, data: { confirm: 'Are you sure?' } %></div>
+        </div>
+      </div>
+<% end %>
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,40 +1,20 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  # GET /posts
-  # GET /posts.json
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
-  def show
-  end
-
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
   end
 
-  # POST /posts
-  # POST /posts.json
   def create
-    @post = Post.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
+    @post = Post.create(post_params)
+    redirect_to root_path
   end
 
   # PATCH/PUT /posts/1
@@ -51,8 +31,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
   def destroy
     @post.destroy
     respond_to do |format|
