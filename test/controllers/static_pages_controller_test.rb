4cd5090ae6b33bef2cf71bf13a1de6e2e11595f3
Jen Strong <jenniferastrong@gmail.com>
2018-07-14 16:03:11 +0100

    Add static Contact Page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,11 +1,11 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
-  end
+  # test "should get home" do
+  #   get static_pages_home_url
+  #   assert_response :success
+  #   assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+  # end
 
   test "should get help" do
     get static_pages_help_url
@@ -17,5 +17,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_about_url
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 end
